c014f715f806fa014797f9fdc52f8c77bee26bb7
RenzoTM2705 <renzotaypicahuana27@gmail.com>
2025-09-18 17:19:18 -0500

    first commit

---
+++ huriosbackend/src/main/java/com/hurios/huriosbackend/HuriosbackendApplication.java
@@ -0,0 +1,13 @@
+package com.hurios.huriosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuriosbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuriosbackendApplication.class, args);
+	}
+
+}
---
+++ huriosbackend/src/test/java/com/hurios/huriosbackend/HuriosbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurios.huriosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuriosbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
