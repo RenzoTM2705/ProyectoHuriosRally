5f7ead4b1461a0c65552e4e491b9e51c2837203e
AlexanderSantos <alexanderasa0210@gmail.com>
2025-09-21 18:55:33 -0500

    segundo comentario

--- huriosbackend/src/main/java/com/hurios/huriosbackend/HuriosbackendApplication.java
+++ huriosbackend/src/main/java/com/hurios/huriosbackend/HuriosbackendApplication.java
@@ -9,6 +9,7 @@ public class HuriosbackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HuriosbackendApplication.class, args);
 		//hola  diego
+		//segundo comentario
 	}
 
 }
