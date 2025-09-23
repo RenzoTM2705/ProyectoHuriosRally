4904745f387d3c86de22641f143dd9c76a39e3da
RenzoTM2705 <renzotaypicahuana27@gmail.com>
2025-09-22 21:45:27 -0500

    Botón de regresar a iniciar sesión agregado a los formularios

--- huriosfrontend/src/pages/ResetPasswordSend.tsx
+++ huriosfrontend/src/pages/ResetPasswordSend.tsx
@@ -7,9 +7,10 @@ export function ResetPasswordSend() {
             <div className="flex flex-col items-center border-[var(--Primary_5)] gap-5 border-2 rounded-lg p-13 mx-3.5 py-13  sm:py-20 sm:px-17 ">
                 <h1 className="text-3xl text-center">Olvidó su contraseña</h1>
                 <span className="text-center">Ingrese el correo con el que se registró previamente</span>
-                <form className="flex flex-col justify-around gap-10 w-60 sm:w-80">
+                <form className="flex flex-col justify-around gap-5 w-60 sm:w-80">
                     <Input label="Correo" type="email" />
                     <ButtonNormal defaultText="Enviar correo de verificación"></ButtonNormal>
+                    <a href="#" className="underline w-fit hover:text-[var(--Primary_4)]">Volver</a>
                 </form>
             </div>
         </main>
